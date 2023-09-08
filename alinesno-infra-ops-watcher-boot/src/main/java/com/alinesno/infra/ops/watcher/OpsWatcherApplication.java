27babf9e1de08ca0fe42f69120fba9950b4b73c9
switchII <luoandon@gmail.com>
2023-09-09 00:27:41 +0800

    feat:调整项目配置

--- alinesno-infra-ops-watcher-boot/src/main/java/com/alinesno/infra/ops/watcher/OpsWatcherApplication.java
+++ alinesno-infra-ops-watcher-boot/src/main/java/com/alinesno/infra/ops/watcher/OpsWatcherApplication.java
@@ -1,5 +1,6 @@
 package com.alinesno.infra.ops.watcher;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -9,7 +10,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author LuoAnDong
  * @since 2023年8月3日 上午6:23:43
  */
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@MapperScan("com.alinesno.infra.ops.watcher.mapper")
+@SpringBootApplication
 public class OpsWatcherApplication {
 
 	public static void main(String[] args) {
