94ddd55f6e8bf7192409fbb4d04032f74365e247
林晓(Lily) <lilyAip@gmail.com>
2024-12-13 08:44:07 +0800

    feat: 添加操作监控消息管理接口

---
+++ alinesno-infra-ops-watcher-ui-console/src/api/ops/watcher/message.js
@@ -0,0 +1,179 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+// 接口配置项
+var prefix = '/api/infra/ops/watcher/alertMessage/' ;
+var managerUrl = {
+    datatables: prefix + "datatables",
+    createUrl: prefix + 'add',
+    saveUrl: prefix + 'save',
+    updateUrl: prefix + "modify",
+    statusUrl: prefix + "changeStatus",
+    cleanUrl: prefix + "clean",
+    detailUrl: prefix + "detail",
+    removeUrl: prefix + "delete",
+    choiceProject: prefix + "choiceProject",
+    exportUrl: prefix + "exportExcel",
+    changeField: prefix + "changeField",
+    currentProject: prefix + "currentProject",
+    downloadfile: prefix + "downloadfile" ,
+    latestList: prefix + "latestList"
+}
+
+// 获取最新的应用列表
+export function getLatestList(){
+    return request({
+        url: managerUrl.latestList ,
+        method: 'get'
+    })
+}
+
+// 修改字段
+export function changStatusField(data){
+    return request({
+        url: managerUrl.changeField ,
+        method: 'post',
+        data: data
+    })
+}
+
+// 选择当前应用
+export function getCurrentProject(){
+    return request({
+        url: managerUrl.currentProject ,
+        method: 'get'
+    })
+}
+
+// 配置当前应用
+export function choiceProject(id){
+    return request({
+        url: managerUrl.choiceProject + '?projectId=' + parseStrEmpty(id),
+        method: 'get'
+    })
+}
+
+// 查询用户列表
+export function listProject(query) {
+    return request({
+        url: managerUrl.datatables,
+        method: 'post',
+        params: query
+    })
+}
+
+// 查询用户详细
+export function getProject(id) {
+    return request({
+        url: managerUrl.detailUrl + '/' + parseStrEmpty(id),
+        method: 'get'
+    })
+}
+
+// 新增用户
+export function addProject(data) {
+    return request({
+        url: managerUrl.saveUrl,
+        method: 'post',
+        data: data
+    })
+}
+
+// 修改用户
+export function updateProject(data) {
+    return request({
+        url: managerUrl.updateUrl,
+        method: 'put',
+        data: data
+    })
+}
+
+// 删除用户
+export function delProject(id) {
+    return request({
+        url: managerUrl.removeUrl + '/' + parseStrEmpty(id),
+        method: 'delete'
+    })
+}
+
+// 用户密码重置
+export function resetProjectPwd(userId, password) {
+    const data = {
+        userId,
+        password
+    }
+    return request({
+        url: '/system/user/resetPwd',
+        method: 'put',
+        data: data
+    })
+}
+
+// 用户状态修改
+export function changeProjectStatus(userId, status) {
+    const data = {
+        userId,
+        status
+    }
+    return request({
+        url: '/system/user/changeStatus',
+        method: 'put',
+        data: data
+    })
+}
+
+// 查询用户个人信息
+export function getProjectProfile() {
+    return request({
+        url: '/system/user/profile',
+        method: 'get'
+    })
+}
+
+// 修改用户个人信息
+export function updateProjectProfile(data) {
+    return request({
+        url: '/system/user/profile',
+        method: 'put',
+        data: data
+    })
+}
+
+// 用户密码重置
+export function updateProjectPwd(oldPassword, newPassword) {
+    const data = {
+        oldPassword,
+        newPassword
+    }
+    return request({
+        url: '/system/user/profile/updatePwd',
+        method: 'put',
+        params: data
+    })
+}
+
+// 用户头像上传
+export function uploadAvatar(data) {
+    return request({
+        url: '/system/user/profile/avatar',
+        method: 'post',
+        data: data
+    })
+}
+
+// 查询授权角色
+export function getAuthRole(userId) {
+    return request({
+        url: '/system/user/authRole/' + userId,
+        method: 'get'
+    })
+}
+
+// 保存授权角色
+export function updateAuthRole(data) {
+    return request({
+        url: '/system/user/authRole',
+        method: 'put',
+        params: data
+    })
+}
