aa9de09c11923b060a8c9f2c621764f256e39276
陈伟(Victor) <victorAip@gmail.com>
2024-12-14 00:33:04 +0800

    refactor(OpsWatcherApplication): 移除CommandLineRunner实现及初始化逻辑

--- alinesno-infra-ops-watcher-boot/src/main/java/com/alinesno/infra/ops/watcher/OpsWatcherApplication.java
+++ alinesno-infra-ops-watcher-boot/src/main/java/com/alinesno/infra/ops/watcher/OpsWatcherApplication.java
@@ -8,27 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 集成一个Java开发示例工具
- * @author LuoAnDong
- * @since 2023年8月3日 上午6:23:43
+ * @author luoxiaodong
+ * @since 1.0.0
  */
 @SpringBootApplication
-public class OpsWatcherApplication implements CommandLineRunner {
+public class OpsWatcherApplication {
 
-	@Autowired
-	private InitService initService ;
-
-	/**
-	 * 程序的入口点。
-	 * 使用SpringProject.run()方法启动Spring Boot应用程序。
-	 * 参数:
-	 * args - 命令行参数，用于配置应用程序的行为。
-	 */
 	public static void main(String[] args) {
 	    SpringApplication.run(OpsWatcherApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		initService.initData() ;
-	}
 }
