bad81fef34797d45d0397810095a6fcf1d52d3e5
陈伟(Victor) <victorAip@gmail.com>
2024-12-14 00:33:04 +0800

    修复 ActableConfigurationSelector 类的注释并恢复包声明

--- alinesno-infra-ops-watcher-boot/src/main/java/com/alinesno/infra/ops/watcher/config/ActableConfigurationSelector.java
+++ alinesno-infra-ops-watcher-boot/src/main/java/com/alinesno/infra/ops/watcher/config/ActableConfigurationSelector.java
@@ -1,50 +1,50 @@
-package com.alinesno.infra.ops.watcher.config;
-
-import com.alinesno.infra.common.facade.enable.ConfigRegisterBean;
-import com.gitee.sunchenbin.mybatis.actable.base.BaseCRUDManagerImpl;
-import com.gitee.sunchenbin.mybatis.actable.database.mysql.system.SysMysqlCreateTableManagerImpl;
-import com.gitee.sunchenbin.mybatis.actable.database.oracle.system.SysOracleCreateTableManagerImpl;
-import com.gitee.sunchenbin.mybatis.actable.database.postgresql.system.SysPostgreSqlCreateTableManagerImpl;
-import com.gitee.sunchenbin.mybatis.actable.startup.StartUpHandlerImpl;
-import com.gitee.sunchenbin.mybatis.actable.utils.ConfigurationUtil;
-import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.context.annotation.ImportSelector;
-import org.springframework.core.type.AnnotationMetadata;
-
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * 引入自动类
- *
- * @author luoxiaodong
- * @version 1.0.0
- */
-@Slf4j
-public class ActableConfigurationSelector implements ImportSelector {
-
-    @NotNull
-    @Override
-    public String[] selectImports(@NotNull AnnotationMetadata importingClassMetadata) {
-
-        List<String> importBean = new ArrayList<>();
-
-        // 引入dao层
-        importBean.add(BaseCRUDManagerImpl.class.getName()) ;
-        importBean.add(ConfigRegisterBean.class.getName()) ;
-
-        // 引入manager
-        importBean.add(StartUpHandlerImpl.class.getName()) ;
-
-        // 引入数据库配置
-        importBean.add(SysMysqlCreateTableManagerImpl.class.getName()) ;
-        importBean.add(SysPostgreSqlCreateTableManagerImpl.class.getName()) ;
-        importBean.add(SysOracleCreateTableManagerImpl.class.getName()) ;
-
-        importBean.add(ConfigurationUtil.class.getName()) ;
-
-        return importBean.toArray(new String[] {});
-    }
-
-}
+//package com.alinesno.infra.ops.watcher.config;
+//
+//import com.alinesno.infra.common.facade.enable.ConfigRegisterBean;
+//import com.gitee.sunchenbin.mybatis.actable.base.BaseCRUDManagerImpl;
+//import com.gitee.sunchenbin.mybatis.actable.database.mysql.system.SysMysqlCreateTableManagerImpl;
+//import com.gitee.sunchenbin.mybatis.actable.database.oracle.system.SysOracleCreateTableManagerImpl;
+//import com.gitee.sunchenbin.mybatis.actable.database.postgresql.system.SysPostgreSqlCreateTableManagerImpl;
+//import com.gitee.sunchenbin.mybatis.actable.startup.StartUpHandlerImpl;
+//import com.gitee.sunchenbin.mybatis.actable.utils.ConfigurationUtil;
+//import lombok.extern.slf4j.Slf4j;
+//import org.jetbrains.annotations.NotNull;
+//import org.springframework.context.annotation.ImportSelector;
+//import org.springframework.core.type.AnnotationMetadata;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+///**
+// * 引入自动类
+// *
+// * @author luoxiaodong
+// * @version 1.0.0
+// */
+//@Slf4j
+//public class ActableConfigurationSelector implements ImportSelector {
+//
+//    @NotNull
+//    @Override
+//    public String[] selectImports(@NotNull AnnotationMetadata importingClassMetadata) {
+//
+//        List<String> importBean = new ArrayList<>();
+//
+//        // 引入dao层
+//        importBean.add(BaseCRUDManagerImpl.class.getName()) ;
+//        importBean.add(ConfigRegisterBean.class.getName()) ;
+//
+//        // 引入manager
+//        importBean.add(StartUpHandlerImpl.class.getName()) ;
+//
+//        // 引入数据库配置
+//        importBean.add(SysMysqlCreateTableManagerImpl.class.getName()) ;
+//        importBean.add(SysPostgreSqlCreateTableManagerImpl.class.getName()) ;
+//        importBean.add(SysOracleCreateTableManagerImpl.class.getName()) ;
+//
+//        importBean.add(ConfigurationUtil.class.getName()) ;
+//
+//        return importBean.toArray(new String[] {});
+//    }
+//
+//}
