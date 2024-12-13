7a653c58a543e733059aa306d55954548ab51ead
陈伟(Victor) <victorAip@gmail.com>
2024-12-14 00:33:04 +0800

    移除配置类并注释相关代码

--- alinesno-infra-ops-watcher-boot/src/main/java/com/alinesno/infra/ops/watcher/config/ConfigRegisterBean.java
+++ alinesno-infra-ops-watcher-boot/src/main/java/com/alinesno/infra/ops/watcher/config/ConfigRegisterBean.java
@@ -1,17 +1,17 @@
-package com.alinesno.infra.ops.watcher.config;
-
-import org.mybatis.spring.mapper.MapperScannerConfigurer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ConfigRegisterBean {
-
-    @Bean
-    public MapperScannerConfigurer mapperScannerConfigurer() {
-        MapperScannerConfigurer configure = new MapperScannerConfigurer();
-        configure.setBasePackage("com.gitee.sunchenbin.mybatis.actable.dao.*");
-        return configure;
-    }
-
-}
+//package com.alinesno.infra.ops.watcher.config;
+//
+//import org.mybatis.spring.mapper.MapperScannerConfigurer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ConfigRegisterBean {
+//
+//    @Bean
+//    public MapperScannerConfigurer mapperScannerConfigurer() {
+//        MapperScannerConfigurer configure = new MapperScannerConfigurer();
+//        configure.setBasePackage("com.gitee.sunchenbin.mybatis.actable.dao.*");
+//        return configure;
+//    }
+//
+//}
