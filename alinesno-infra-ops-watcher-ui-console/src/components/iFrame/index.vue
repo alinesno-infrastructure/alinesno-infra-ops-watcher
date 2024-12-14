ea32dd748bb8bd453286220cdfd4b112ca89a820
李伟(Michael) <michaelAip@gmail.com>
2024-12-15 00:32:00 +0800

    优化:iFrame组件样式并添加加载动画

--- alinesno-infra-ops-watcher-ui-console/src/components/iFrame/index.vue
+++ alinesno-infra-ops-watcher-ui-console/src/components/iFrame/index.vue
@@ -1,14 +1,25 @@
 <template>
-  <div v-loading="loading" :style="'height:' + height">
-    <iframe 
-      :src="url" 
-      frameborder="no" 
-      style="width: 100%; height: 100%" 
-      scrolling="auto" />
+<!--  <div v-loading="loading" :style="'height:' + height">-->
+<!--    <iframe -->
+<!--      :src="url" -->
+<!--      frameborder="no" -->
+<!--      style="width: 100%; height: 100%" -->
+<!--      scrolling="auto" />-->
+<!--  </div>-->
+  <div :style="'height:' + height">
+    <iframe
+        :src="url"
+        frameborder="no"
+        style="width: 100%; height: 100%;border-radius: 3px"
+        scrolling="auto" />
   </div>
 </template>
 
 <script setup>
+
+import { ElLoading } from 'element-plus'
+import {onMounted} from "vue";
+
 const props = defineProps({
   src: {
     type: String,
@@ -16,14 +27,28 @@ const props = defineProps({
   }
 })
 
-const height = ref(document.documentElement.clientHeight - 94.5 + "px;")
-const loading = ref(true)
+const height = ref(document.documentElement.clientHeight - 60 + "px;")
+// const loading = ref(true)
 const url = computed(() => props.src)
 
-onMounted(() => {
+
+const openFullScreen2 = () => {
+  const loading = ElLoading.service({
+    lock: true,
+    text: '加载监管中...',
+    background: 'rgba(0, 0, 0, 0.7)',
+  })
   setTimeout(() => {
-    loading.value = false;
-  }, 2000);
+    loading.close()
+  }, 500)
+}
+
+onMounted(() => {
+  // setTimeout(() => {
+  //   // loading.value = false;
+  //   loading.close() ;
+  // }, 2000);
+  openFullScreen2();
   window.onresize = function temp() {
     height.value = document.documentElement.clientHeight - 94.5 + "px;";
   };
