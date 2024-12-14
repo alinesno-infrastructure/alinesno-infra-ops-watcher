75d04bdf1d341be80a6c3ca98e51dec8c4480abe
李伟(Michael) <michaelAip@gmail.com>
2024-12-15 00:32:00 +0800

    fix:修正OpsWatcherApplication注释格式问题

--- alinesno-infra-ops-watcher-boot/src/main/java/com/alinesno/infra/ops/watcher/OpsWatcherApplication.java
+++ alinesno-infra-ops-watcher-boot/src/main/java/com/alinesno/infra/ops/watcher/OpsWatcherApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
+/**al
  * 集成一个Java开发示例工具
  * @author luoxiaodong
  * @since 1.0.0
