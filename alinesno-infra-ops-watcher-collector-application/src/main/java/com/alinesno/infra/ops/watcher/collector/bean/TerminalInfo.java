874d0fac189e55179075280e3da879d05f678a54
李娜(Allen) <allenAip@gmail.com>
2024-12-16 00:33:11 +0800

    feat:新增 TerminalInfo 类，包含终端相关的信息字段。

---
+++ alinesno-infra-ops-watcher-collector-application/src/main/java/com/alinesno/infra/ops/watcher/collector/bean/TerminalInfo.java
@@ -0,0 +1,56 @@
+package com.alinesno.infra.ops.watcher.collector.bean;
+
+import lombok.Data;
+
+@Data
+public class TerminalInfo {
+    /**
+     * 终端分辨率。
+     */
+    private String resolvingPower;
+
+    /**
+     * 文档来源页面的URL。
+     */
+    private String referrer;
+
+    /**
+     * 用户代理字符串。
+     */
+    private String ua;
+
+    /**
+     * 浏览器信息。
+     */
+    private BrowserInfo browser;
+
+    /**
+     * 浏览器引擎信息。
+     */
+    private EngineInfo engine;
+
+    /**
+     * 操作系统信息。
+     */
+    private OsInfo os;
+
+    /**
+     * 设备信息。
+     */
+    private DeviceInfo device;
+
+    /**
+     * CPU信息。
+     */
+    private CpuInfo cpu;
+
+    /**
+     * IP地址。
+     */
+    private String ip;
+
+    /**
+     * 城市信息。
+     */
+    private String city;
+}
