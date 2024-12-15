71d61f2e0c531e9e5433c67399e2961680e93abc
李娜(Allen) <allenAip@gmail.com>
2024-12-16 00:33:11 +0800

    feat: 添加 MessageEventFactory 类，实现 EventFactory 接口用于创建 MessageEvent 实例。

---
+++ alinesno-infra-ops-watcher-collector-application/src/main/java/com/alinesno/infra/ops/watcher/collector/queue/MessageEventFactory.java
@@ -0,0 +1,11 @@
+//package com.alinesno.infra.ops.watcher.collector.queue;
+//
+//import com.lmax.disruptor.EventFactory;
+//
+//public class MessageEventFactory implements EventFactory<MessageEvent> {
+//
+//    @Override
+//    public MessageEvent newInstance() {
+//        return new MessageEvent();
+//    }
+//}
