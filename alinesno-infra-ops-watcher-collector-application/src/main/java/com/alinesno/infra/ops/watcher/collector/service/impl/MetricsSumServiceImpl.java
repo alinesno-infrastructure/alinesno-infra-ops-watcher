895a910936a9f85ae5b58441561174111ff038f7
李娜(Allen) <allenAip@gmail.com>
2024-12-17 00:34:28 +0800

    fix:完善 MetricsSumServiceImpl 中的参数设置及日志处理

--- alinesno-infra-ops-watcher-collector-application/src/main/java/com/alinesno/infra/ops/watcher/collector/service/impl/MetricsSumServiceImpl.java
+++ alinesno-infra-ops-watcher-collector-application/src/main/java/com/alinesno/infra/ops/watcher/collector/service/impl/MetricsSumServiceImpl.java
@@ -1,5 +1,6 @@
 package com.alinesno.infra.ops.watcher.collector.service.impl;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alinesno.infra.ops.watcher.collector.service.IMetricsSumService;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +16,7 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.alinesno.infra.ops.watcher.collector.enums.ClickhouseSql.metricsSumSql;
 
@@ -38,83 +40,99 @@ public class MetricsSumServiceImpl implements IMetricsSumService {
         PreparedStatement statement = null;
         try {
             connection = sqlSessionTemplate.getConnection();
-
-            // 关闭自动提交
-            // connection.setAutoCommit(false);
-
+            // SQL 语句中的字段顺序与下面设置参数的顺序一致
+            
             statement = connection.prepareStatement(metricsSumSql);
 
-//            JSONArray jsonArray = JSON.parseArray(logList);
-            for(Map<String, Object> map : logList) {
+            for (Map<String, Object> map : logList) {
+
                 JSONObject jsonObject = new JSONObject(map);
 
+                // 设置 resource_attributes 参数
                 JSONObject resourceAttributes = jsonObject.getJSONObject("resourceAttributes");
-                Map<String, String> resAttr = new HashMap<>();
-                for (String key : resourceAttributes.keySet()) {
-                    String value = resourceAttributes.getString(key);
-                    resAttr.put(key, value);
-                }
+                Map<String, String> resAttr = convertJsonToMap(resourceAttributes);
                 statement.setObject(1, resAttr);
+
+                // 设置其他字符串类型参数
                 statement.setString(2, jsonObject.getString("resourceSchemaUrl"));
                 statement.setString(3, jsonObject.getString("scopeName"));
                 statement.setString(4, jsonObject.getString("scopeVersion"));
+
+                // 设置 scope_attributes 参数
                 JSONObject scopeAttributes = jsonObject.getJSONObject("scopeAttributes");
-                Map<String, String> scoAttr = new HashMap<>();
-                for (String key : scopeAttributes.keySet()) {
-                    String value = scopeAttributes.getString(key);
-                    scoAttr.put(key, value);
-                }
+                Map<String, String> scoAttr = convertJsonToMap(scopeAttributes);
                 statement.setObject(5, scoAttr);
-                statement.setString(6, jsonObject.getString("scopeDroppedAttrCount"));
+
+                // 设置整数类型参数
+                statement.setInt(6, jsonObject.getInteger("scopeDroppedAttrCount"));
+
+                // 设置其他字符串类型参数
                 statement.setString(7, jsonObject.getString("scopeSchemaUrl"));
                 statement.setString(8, jsonObject.getString("metricName"));
                 statement.setString(9, jsonObject.getString("metricDescription"));
                 statement.setString(10, jsonObject.getString("metricUnit"));
-                JSONObject attributes = jsonObject.getJSONObject("attributes");
-                Map<String, String> attr = new HashMap<>();
-                for (String key : attributes.keySet()) {
-                    String value = attributes.getString(key);
-                    attr.put(key, value);
-                }
-                statement.setObject(11, attr);
-                statement.setString(12, jsonObject.getString("startTimeUnix"));
-                statement.setString(13, jsonObject.getString("timeUnix"));
-                statement.setString(14, jsonObject.getString("value"));
-                statement.setLong(15, jsonObject.getLong("flags"));
 
-                // TODO exemplarsFilteredAttributes 数据为 Map 类型，但是数据库对应字段类型为 Array，调整 exemplarsFilteredAttributes 数据以正确插入数据库中
-//                statement.setString(16, jsonObject.getString("exemplarsFilteredAttributes"));
-//
-//                statement.setObject(17, jsonObject.getString("exemplarsTimeUnix"));
-//                statement.setString(18, jsonObject.getString("exemplarsValue"));
-//                statement.setString(19, jsonObject.getString("exemplarsSpanId"));
-//                statement.setString(20, jsonObject.getString("exemplarsTraceId"));
-//
-//                // TODO aggTemp 的类型为 AggregationTemporality，数据库对应字段的类型为 Int32，调整aggTemp数据以正确插入数据库
-//                statement.setString(21, jsonObject.getString("aggTemp"));
-//
-//                statement.setString(22, jsonObject.getString("ssMonotonic"));
+                // 设置 attributes 参数
+                JSONObject attributes = jsonObject.getJSONObject("attributes");
+                Map<String, String> attr = convertJsonToMap(attributes);
+                statement.setObject(11, attr);
+
+                // 设置时间戳类型参数
+                statement.setLong(12, jsonObject.getLong("startTimeUnix"));
+                statement.setLong(13, jsonObject.getLong("timeUnix"));
+
+                // 设置数值类型参数
+                statement.setDouble(14, jsonObject.getDoubleValue("value"));
+
+                // 设置 flags 参数
+                statement.setInt(15, jsonObject.getInteger("flags"));
 
                 statement.addBatch();
-                int[] result = statement.executeBatch();
-
-                // 提交事务
-                // connection.commit();
-
-                log.debug("数据插入成功: {}", result);
             }
+
+            int[] result = statement.executeBatch();
+            log.debug("数据插入成功: {}", result);
 
             statement.close();
         } catch (SQLException e) {
             log.error("数据插入异常: {}", e.getMessage());
-            // 出现异常时回滚事务
             try {
-                // connection.rollback();
                 connection.close();
             } catch (SQLException ex) {
-                log.error("数据插入回滚异常: {}", ex.getMessage());
+                log.error("关闭连接异常: {}", ex.getMessage());
             }
             throw new RuntimeException(e);
+        } finally {
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    log.error("关闭 PreparedStatement 异常: {}", e.getMessage());
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    log.error("关闭 Connection 异常: {}", e.getMessage());
+                }
+            }
         }
     }
+
+    // 辅助方法：将 JSONObject 转换为 Map<String, String>
+    private static Map<String, String> convertJsonToMap(JSONObject jsonObject) {
+        Map<String, String> resultMap = new HashMap<>();
+        for (String key : jsonObject.keySet()) {
+            Object value = jsonObject.get(key);
+            // 如果值是 JSONArray 或者 JSONObject，则转换成字符串
+            if (value instanceof JSONObject || value instanceof JSONArray) {
+                resultMap.put(key, value.toString());
+            } else {
+                resultMap.put(key, String.valueOf(value));
+            }
+        }
+        return resultMap;
+    }
+
 }
