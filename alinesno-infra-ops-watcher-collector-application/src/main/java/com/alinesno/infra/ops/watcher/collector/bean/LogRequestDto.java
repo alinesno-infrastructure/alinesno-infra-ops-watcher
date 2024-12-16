00cd81ab5b0a5e07f76f532ff77dc949555dace0
李娜(Allen) <allenAip@gmail.com>
2024-12-17 00:34:28 +0800

    feat:添加 LogRequestDto 类，包含点击消息列表及终端和性能信息字段调整。

--- alinesno-infra-ops-watcher-collector-application/src/main/java/com/alinesno/infra/ops/watcher/collector/bean/LogRequestDto.java
+++ alinesno-infra-ops-watcher-collector-application/src/main/java/com/alinesno/infra/ops/watcher/collector/bean/LogRequestDto.java
@@ -5,20 +5,31 @@ import java.util.List;
 
 @Data
 public class LogRequestDto {
+
+    /**
+     * 点击消息列表。
+     */
+    private List<ClickLogEntry> click_list;
+
     /**
      * IO消息列表，继承自PerformanceEntry。
      */
-    private List<IoLogEntry> ioList;
+    private List<IoLogEntry> io_list;
+
+    /**
+     * 自定义日志条目列表。
+     */
+    private List<DiyLogEntry> diy_list;
 
     /**
      * 终端信息。
      */
-    private TerminalInfo terminalInfo;
+    private TerminalInfo terminal_info;
 
     /**
      * 性能信息。
      */
-    private PerformanceInfo performanceInfo;
+    private PerformanceInfo performance_info;
 
     /**
      * 用户唯一标识符，由IP与用户代理生成。
