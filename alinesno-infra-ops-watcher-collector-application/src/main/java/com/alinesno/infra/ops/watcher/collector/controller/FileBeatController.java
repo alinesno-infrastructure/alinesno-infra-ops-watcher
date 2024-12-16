895ac6639af3b1018d1f738c2391d439e9c619e2
李娜(Allen) <allenAip@gmail.com>
2024-12-17 00:34:27 +0800

    feat:新增 FileBeatController 类，用于接收 filebeat 日志数据。

---
+++ alinesno-infra-ops-watcher-collector-application/src/main/java/com/alinesno/infra/ops/watcher/collector/controller/FileBeatController.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.ops.watcher.collector.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 接收filebeat日志数据结构
+ */
+@RestController
+@RequestMapping("/api/filebeat")
+public class FileBeatController {
+
+
+}
