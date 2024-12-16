dde5567ddd3fff0c79643b35b68aa19471918be5
李娜(Allen) <allenAip@gmail.com>
2024-12-17 00:34:28 +0800

    改进 FontLoggerController 使用 AjaxResult 替代 ResponseEntity 返回处理结果。

--- alinesno-infra-ops-watcher-collector-application/src/main/java/com/alinesno/infra/ops/watcher/collector/controller/FontLoggerController.java
+++ alinesno-infra-ops-watcher-collector-application/src/main/java/com/alinesno/infra/ops/watcher/collector/controller/FontLoggerController.java
@@ -1,9 +1,9 @@
 package com.alinesno.infra.ops.watcher.collector.controller;
 
+import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.ops.watcher.collector.bean.LogRequestDto;
 import com.alinesno.infra.ops.watcher.collector.service.LogHandlerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,11 +23,11 @@ public class FontLoggerController {
      * @return 返回处理结果。
      */
     @PostMapping("/collect")
-    public ResponseEntity<String> collectLogs(@RequestBody LogRequestDto logRequestDto) {
+    public AjaxResult collectLogs(@RequestBody LogRequestDto logRequestDto) {
         // Pass the received DTO to the handler service for processing.
         logHandlerService.handleLog(logRequestDto);
 
         // Return success response or handle errors as appropriate.
-        return ResponseEntity.ok("Logs collected successfully.");
+        return AjaxResult.success() ;
     }
 }
