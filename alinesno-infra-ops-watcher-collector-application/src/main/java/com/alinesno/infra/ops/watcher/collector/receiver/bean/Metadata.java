d42bad14e6cc46f64eceb27a2d5d8b4633691b46
李娜(Allen) <allenAip@gmail.com>
2024-12-17 00:34:27 +0800

    feat: 添加 Metadata 类，包含基本属性 beat、type 和 version。

---
+++ alinesno-infra-ops-watcher-collector-application/src/main/java/com/alinesno/infra/ops/watcher/collector/receiver/bean/Metadata.java
@@ -0,0 +1,18 @@
+package com.alinesno.infra.ops.watcher.collector.receiver.bean;
+
+import lombok.Data;
+
+@Data
+public class Metadata {
+    private String beat;
+    private String type;
+    private String version;
+}
+
+
+
+
+
+
+
+
