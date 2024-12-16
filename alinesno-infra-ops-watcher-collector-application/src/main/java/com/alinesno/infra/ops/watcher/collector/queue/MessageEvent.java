5bf53d7dd3c8d7e5a859c663a1ac1c90832753a7
李娜(Allen) <allenAip@gmail.com>
2024-12-17 00:34:27 +0800

    更新:alinesno-infra-ops-watcher-collector-application/src/main/java/com/alinesno/infra/ops/watcher/collector/queue/MessageEvent.java

--- alinesno-infra-ops-watcher-collector-application/src/main/java/com/alinesno/infra/ops/watcher/collector/queue/MessageEvent.java
+++
@@ -1,9 +0,0 @@
-//package com.alinesno.infra.ops.watcher.collector.queue;
-//
-//import lombok.Data;
-//
-//@Data
-//public class MessageEvent {
-//    private String message; // 消息内容
-//    private String topic ; // 队列名称
-//}
