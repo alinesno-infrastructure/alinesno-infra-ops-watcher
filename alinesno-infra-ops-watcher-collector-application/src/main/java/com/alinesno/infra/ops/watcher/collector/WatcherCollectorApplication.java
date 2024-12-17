590ca54e586ead5e680cc692bf589a2042d44dfe
李娜(Allen) <allenAip@gmail.com>
2024-12-18 00:33:09 +0800

    feat: 添加 WatcherCollectorApplication 类以启动 OTLP OpenTelemetry 服务器和服务收集功能。

---
+++ alinesno-infra-ops-watcher-collector-application/src/main/java/com/alinesno/infra/ops/watcher/collector/WatcherCollectorApplication.java
@@ -0,0 +1,49 @@
+package com.alinesno.infra.ops.watcher.collector;
+
+import com.alinesno.infra.common.web.adapter.config.CORSProperites;
+import com.alinesno.infra.common.web.adapter.config.CorsFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+
+/**
+ * 描述：该类表示一个OTLP OpenTelemetry服务器，用于接收遥测数据。
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@SpringBootApplication
+public class WatcherCollectorApplication extends TelemetryServer implements CommandLineRunner  {
+
+    /**
+     * 应用程序的入口点。
+     *
+     * @param args 命令行参数。
+     */
+    public static void main(String[] args) throws InterruptedException, IOException {
+        SpringApplication.run(WatcherCollectorApplication.class, args);
+        TelemetryServer otlpServer = new TelemetryServer();
+        otlpServer.start();
+        otlpServer.blockUntilShutdown();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.debug("应用收集服务启动 ...");
+    }
+
+    @Bean
+    public CORSProperites getCORSProperites(){
+        return new CORSProperites() ;
+    }
+
+    @Bean
+    public CorsFilter getCorsFilter(){
+        return new CorsFilter() ;
+    }
+}
