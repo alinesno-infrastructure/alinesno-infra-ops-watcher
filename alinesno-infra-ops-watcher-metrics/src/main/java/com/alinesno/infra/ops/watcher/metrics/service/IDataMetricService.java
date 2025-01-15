6a50ee4e3dd8016e0c8d38b6ef9514931421b1d1
王磊(Levi) <leviAip@gmail.com>
2025-01-16 00:33:32 +0800

    feat:新增 IDataMetricService 接口，定义数据监控指标服务的基本操作。

---
+++ alinesno-infra-ops-watcher-metrics/src/main/java/com/alinesno/infra/ops/watcher/metrics/service/IDataMetricService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.ops.watcher.metrics.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.ops.watcher.metrics.entity.DataMetricEntity;
+
+public interface IDataMetricService extends IBaseService<DataMetricEntity> {
+}
