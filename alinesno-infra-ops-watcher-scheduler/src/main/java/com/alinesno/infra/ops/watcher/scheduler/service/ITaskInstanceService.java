6c908f64dda955edf3e7d7dc0aad8a1b973f687e
王磊(Levi) <leviAip@gmail.com>
2025-01-16 00:33:32 +0800

    feat:新增 ITaskInstanceService 接口，定义任务实例服务的基本操作。

---
+++ alinesno-infra-ops-watcher-scheduler/src/main/java/com/alinesno/infra/ops/watcher/scheduler/service/ITaskInstanceService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.ops.watcher.scheduler.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.ops.watcher.scheduler.entity.TaskInstanceEntity;
+
+public interface ITaskInstanceService extends IBaseService<TaskInstanceEntity> {
+}
