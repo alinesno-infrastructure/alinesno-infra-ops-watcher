336209fce80f21e5a9e131c5b711f1eb12307833
王磊(Levi) <leviAip@gmail.com>
2025-01-16 00:33:32 +0800

    feat:新增 IResourceService 接口，定义资源服务的基本操作。

---
+++ alinesno-infra-ops-watcher-scheduler/src/main/java/com/alinesno/infra/ops/watcher/scheduler/service/IResourceService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.ops.watcher.scheduler.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.ops.watcher.scheduler.entity.ResourceEntity;
+
+public interface IResourceService extends IBaseService<ResourceEntity> {
+}
