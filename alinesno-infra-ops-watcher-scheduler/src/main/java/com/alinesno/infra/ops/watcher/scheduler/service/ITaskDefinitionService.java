236ecbbe8e6e6758e36c991a800cd5d6c88eedba
王磊(Levi) <leviAip@gmail.com>
2025-01-16 00:33:32 +0800

    feat:新增 ITaskDefinitionService 接口

---
+++ alinesno-infra-ops-watcher-scheduler/src/main/java/com/alinesno/infra/ops/watcher/scheduler/service/ITaskDefinitionService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.ops.watcher.scheduler.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.ops.watcher.scheduler.entity.TaskDefinitionEntity;
+
+public interface ITaskDefinitionService extends IBaseService<TaskDefinitionEntity> {
+}
