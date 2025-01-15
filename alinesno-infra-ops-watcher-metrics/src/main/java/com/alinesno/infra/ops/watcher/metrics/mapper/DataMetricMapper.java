48f70d2d608e1c5fc91da1bc66a882c9cb4c07d7
王磊(Levi) <leviAip@gmail.com>
2025-01-16 00:33:32 +0800

    feat:新增 DataMetricMapper 接口，实现基本映射功能。

---
+++ alinesno-infra-ops-watcher-metrics/src/main/java/com/alinesno/infra/ops/watcher/metrics/mapper/DataMetricMapper.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.ops.watcher.metrics.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.ops.watcher.metrics.entity.DataMetricEntity;
+
+public interface DataMetricMapper extends IBaseMapper<DataMetricEntity> {
+}
