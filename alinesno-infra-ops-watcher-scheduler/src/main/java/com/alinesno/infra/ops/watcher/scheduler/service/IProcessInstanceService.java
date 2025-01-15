3e89d4e563c1aab2738be33b8ccca917a332790a
王磊(Levi) <leviAip@gmail.com>
2025-01-16 00:33:32 +0800

    feat:新增 IProcessInstanceService 接口文件

---
+++ alinesno-infra-ops-watcher-scheduler/src/main/java/com/alinesno/infra/ops/watcher/scheduler/service/IProcessInstanceService.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.ops.watcher.scheduler.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.ops.watcher.scheduler.entity.ProcessInstanceEntity;
+
+/**
+ */
+public interface IProcessInstanceService extends IBaseService<ProcessInstanceEntity> {
+
+//    /**
+//     * 读取日志
+//     * @param processInstanceId
+//     * @return
+//     */
+//    LogReadResultDto readLog(long processInstanceId , String start);
+
+}
