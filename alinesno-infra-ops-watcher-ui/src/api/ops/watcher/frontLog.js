459a518c6ab4855cd2428eafb66afc8cbe7ae555
陈伟(Victor) <victorAip@gmail.com>
2025-01-24 00:30:37 +0800

    feat:新增 frontLog.js 文件，包含项目管理相关接口调用

---
+++ alinesno-infra-ops-watcher-ui/src/api/ops/watcher/frontLog.js
@@ -0,0 +1,96 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+
+// 接口配置项
+var prefix = '/api/infra/ops/watcher/project/' ;
+var managerUrl = {
+  datatables : prefix +"datatables" ,
+  createUrl: prefix + 'add' ,
+  saveUrl: prefix + 'save' ,
+  updateUrl: prefix +"modify" ,
+  statusUrl: prefix +"changeStatus" ,
+  cleanUrl: prefix + "clean",
+  detailUrl: prefix +"detail",
+  removeUrl: prefix + "delete" ,
+  exportUrl: prefix + "exportExcel",
+  changeField: prefix + "changeField",
+  downloadfile: prefix + "downloadfile"
+}
+
+// 查询应用列表
+export function listProject(query) {
+  return request({
+    url: managerUrl.datatables ,
+    method: 'post',
+    params: query
+  })
+}
+
+// 查询应用详细
+export function getProject(id) {
+  return request({
+    url: managerUrl.detailUrl + '/' + parseStrEmpty(id),
+    method: 'get'
+  })
+}
+
+// 新增应用
+export function addProject(data) {
+  return request({
+    url: managerUrl.saveUrl ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改应用
+export function updateProject(data) {
+  return request({
+    url: managerUrl.updateUrl ,
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除应用
+export function delProject(id) {
+  return request({
+    url: managerUrl.removeUrl + '/' + parseStrEmpty(id),
+    method: 'delete'
+  })
+}
+
+// 应用密码重置
+export function resetProjectPwd(id, password) {
+  const data = {
+    id,
+    password
+  }
+  return request({
+    url: '/api/infra/base/starter/application/resetPwd',
+    method: 'put',
+    data: data
+  })
+}
+
+// 应用状态修改
+export function changeProjectStatus(id, status) {
+  const data = {
+    id,
+    status
+  }
+  return request({
+    url: '/api/infra/base/starter/application/changeStatus',
+    method: 'put',
+    data: data
+  })
+}
+
+// 查询部门下拉树结构
+export function deptTreeSelect() {
+  return request({
+    url: '/api/infra/base/starter/application/deptTree',
+    method: 'get'
+  })
+}
